1529500a1624404beebf3b9bae948376ee3ad330
pavan <pavan@elberttech.com>
2025-01-23 18:15:20 +0530

    changes 3

--- src/test/java/com/example/emailTemplateManagement/EmailTemplateManagementApplicationTests.java
+++ src/test/java/com/example/emailTemplateManagement/EmailTemplateManagementApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.emailTemplateManagement;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EmailTemplateManagementApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.emailTemplateManagement;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EmailTemplateManagementApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
